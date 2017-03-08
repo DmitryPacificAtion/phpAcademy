4be3f0bab540bec87182fedab2250e22d73cd448
Dmitry Bruslik <dmitry.bruslik@yandex.ua>
2017-03-08 23:25:21 +0200

    Домашка 1

---
+++ Backend/php_basics_tasks/23.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 23. Напишите 3 строки, используя три разные функции для вывода текста.
+ */
+
+echo 'This is echo'.'<br>';
+print('This is print'.'<br>');
+var_dump('This is var_dump');
+?>
