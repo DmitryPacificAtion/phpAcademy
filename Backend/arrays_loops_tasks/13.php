cf3132eb233cd66f99a59d39d97fc70652389003
Dima <admin@ukrvet.ua>
2017-03-14 18:05:04 +0200

    Переделал 13, 25 и 26

--- Backend/arrays_loops_tasks/13.php
+++ Backend/arrays_loops_tasks/13.php
@@ -1,13 +1,15 @@
+<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Задачи по массивам и циклам</title></head><body><table border="1px" style="padding:px"><tbody>
 <?php
 //13. Вывести таблицу умножения
 
 for ($i = 1, $j = 1; $i <= 10; ++$i){
-    echo '<pre>';
+    echo '<tr>';
     while ($j <= 10) {
-        echo $i * $j.' ';
+        echo '<td style=\'padding:10px\';>',$i * $j.'</td>';
         $j++;
     }
     $j = 1;
-    echo '<br></pre>';
+     echo '</tr>';
 }
 ?>
+</tbody></table></body></html>
--- Backend/arrays_loops_tasks/25.php
+++ Backend/arrays_loops_tasks/25.php
@@ -2,26 +2,44 @@
 //25. Ваше задание создать массив, наполнить это случайными значениями (функция rand),
 // найти максимальное и минимальное значение и поменять их местами.
 
-$arr = [0];
+$arr = array();
 
 $j = 0;
 while ($j < 20) {
-    $arr[$j] = rand(0, 1000);
-    $j++;
+	$arr[$j] = rand(0, 1000);
+	$j++;
 }
 
-$max = -INF; //  Максимальное число = отрицательная бесконечность
-$min =  INF; //  Минимотное число = положительная бесконечность
-
-foreach ($arr as $k) {
-    if ($k > $max) {
-        $max = $k;
-    }
-    if ($k < $min) {
-        $min = $k;
-    }
+function printArr($arr) {
+	foreach ($arr as $k) {
+		echo $k.'<br>';
+	}
 }
 
-echo "max: $max, min: $min";
+$max = -INF; // Максимальное число = отрицательная бесконечность
+$min =  INF; // Минимотное число = положительная бесконечность
+$minIndx = null;
+$maxIndx = null;
+
+printArr($arr);
+
+foreach ($arr as $k => $v) {
+	if ($v > $max) {
+		$max = $v;
+		$maxIndx = $k;
+	}
+	if ($v < $min) {
+		$min = $v;
+		$minIndx = $k;
+	}
+}
+
+$arr[$minIndx] = $max." - MAX [$minIndx]";
+$arr[$maxIndx] = $min." - MIN [$maxIndx]";
+
+echo '<br>Поменяли местами min и max:<br><br>';
+echo "Было: min: $min [$minIndx], max: $max [$maxIndx]<br>Стало:<br><br>";
+
+printArr($arr);
 
 ?>
